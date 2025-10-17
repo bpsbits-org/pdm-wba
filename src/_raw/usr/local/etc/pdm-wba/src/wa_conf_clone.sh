ff53d853738b55dbf311aa50651ec23c57459c74
bpsbits-org <158743628+bpsbits-org@users.noreply.github.com>
2025-10-17 15:44:54 +0300

    misc
    Signed-off-by: bpsbits-org <158743628+bpsbits-org@users.noreply.github.com>

--- src/_raw/usr/local/etc/pdm-wba/src/wa_conf_clone.sh
+++ src/_raw/usr/local/etc/pdm-wba/src/wa_conf_clone.sh
@@ -14,8 +14,8 @@ copy_conf_files_if_needed(){
     wa_conf_clone "/var/lib/pdm-wba/tpl/wa.conf"
     wa_conf_clone "/etc/fail2ban/jail.d/00-global.local"
     wa_conf_clone "/etc/fail2ban/jail.d/sshd.local"
-    wa_conf_clone "/etc/ssh/sshd_config.d/10-wa-auth.conf"
     wa_conf_clone "/etc/ssh/sshd_config.d/20-wa-access.conf"
+    wa_conf_clone "/etc/ssh/sshd_config.d/50-wa-auth.conf"
 }
 
 # Check if --run argument is provided
--- src/_raw/usr/local/etc/pdm-wba/src/wai_install_service.sh
+++ src/_raw/usr/local/etc/pdm-wba/src/wai_install_service.sh
@@ -4,15 +4,17 @@
 
 # Installs a quadlet service file for WA by moving it to the user's config directory and reloading daemon
 wai_install_service(){
-    local file filename dest_file
+    local file filename dest_file service_name
     file=$1
     filename=$(basename "${file}")
+    service_name="${filename//./-}.service"
     [[ "$file" =~ \.(container|volume|network|pod)$ ]] || return 0
     dest_file="/home/wa/.config/containers/systemd/${filename}"
     if [ -f "${file}" ]; then
         mv "${file}" "${dest_file}"
         wai_fix_owner "${dest_file}"
         systemctl --machine="300@.host" --user daemon-reload
+        systemctl --machine="300@.host" --user start "${service_name}"
         echo "Installed ${filename}"
     fi
 }
