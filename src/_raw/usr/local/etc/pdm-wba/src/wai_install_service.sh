986cac8a7c220ef1dc1da5ca19a10f9be1af7afd
bpsbits-org <158743628+bpsbits-org@users.noreply.github.com>
2025-10-18 15:27:03 +0300

    misc
    Signed-off-by: bpsbits-org <158743628+bpsbits-org@users.noreply.github.com>

--- src/_raw/usr/local/etc/pdm-wba/src/wai_install_service.sh
+++ src/_raw/usr/local/etc/pdm-wba/src/wai_install_service.sh
@@ -3,12 +3,18 @@
 # /usr/local/etc/pdm-wba/src
 
 # Installs a quadlet service file for WA by moving it to the user's config directory and reloading daemon
-wai_install_service(){
+wai_install_service() {
     local file filename dest_file service_name
     file=$1
     filename=$(basename "${file}")
     [[ "$file" =~ \.(container|volume|network|pod)$ ]] || return 0
-    service_name=$(echo "${filename}" | sed -E 's/\.([^.]+)$/-\1.service/')
+    if [[ "$filename" =~ \.container$ ]]; then
+        # For containers
+        service_name="${filename%.container}.service"
+    else
+        # For network and volumes
+        service_name=$(echo "${filename}" | sed -E 's/\.([^.]+)$/-\1.service/')
+    fi
     dest_file="/home/wa/.config/containers/systemd/${filename}"
     if [ -f "${file}" ]; then
         mv "${file}" "${dest_file}"
